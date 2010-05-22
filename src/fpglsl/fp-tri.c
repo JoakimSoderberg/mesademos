3909b6c9c97dc4e2df95e0b5fe22d1dde3fea44e
Brian Paul <brianp@vmware.com>
2010-05-22 07:03:17 -0600

    fp-tri: fix warnings

--- src/fpglsl/fp-tri.c
+++ src/fpglsl/fp-tri.c
@@ -105,7 +105,7 @@ static void check_link(GLuint prog)
    }
 }
 
-static void setup_uniforms()
+static void setup_uniforms(void)
 {
    {
       GLint loc1f = glGetUniformLocationARB(program, "Offset1f");
@@ -158,7 +158,7 @@ static void setup_uniforms()
    }
 }
 
-static void prepare_shaders()
+static void prepare_shaders(void)
 {
    static const char *fragShaderText =
       "void main() {\n"

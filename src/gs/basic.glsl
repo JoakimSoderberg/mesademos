f2f123f880ec4a6abaddfe0a1906aea2f954fd90
Zack Rusin <zack@kde.org>
2010-07-10 18:23:22 -0400

    Add a new example rendering a bezier curve using a geometry shader

--- src/gs/basic.glsl
+++ src/gs/basic.glsl
@@ -1,14 +1,11 @@
 #version 120
 #extension GL_ARB_geometry_shader4 : enable
 
-const int KernelSize = 9;
-
 void main()
 {
   for(int i = 0; i < gl_VerticesIn; ++i)
   {
     gl_FrontColor = gl_FrontColorIn[i];
-    gl_BackColor = gl_BackColorIn[i];
     gl_Position = gl_PositionIn[i];
     EmitVertex();
   }

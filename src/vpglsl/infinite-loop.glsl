cd8c68ca99d687d9a95e8e8798a0e6d13367388f
José Fonseca <jfonseca@vmware.com>
2012-04-02 13:02:08 +0100

    vpglsl: Add an vpglsl infinite loop too.

--- src/vpglsl/infinite-loop.glsl
+++ src/vpglsl/infinite-loop.glsl
@@ -1,7 +1,8 @@
 void main() {
+   gl_Position = gl_Vertex;
    vec4 sum = vec4(0);
    for (int i = 1; i != 2; i += 2) {
       sum += vec4(0.1, 0.1, 0.1, 0.1);
    }
-   gl_FragColor = sum;
+   gl_FrontColor = sum;
 }

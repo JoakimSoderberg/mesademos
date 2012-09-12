b25b762b164bf943fe5a9aa6b086dc8f573d5c77
Brian Paul <brianp@vmware.com>
2012-09-12 13:42:57 -0600

    glsl/identity: clean out unused code
    And set clear color's alpha to 1.0

--- src/glsl/identity.c
+++ src/glsl/identity.c
@@ -14,14 +14,10 @@
 #include "shaderutil.h"
 
 
-static char *FragProgFile = NULL;
-static char *VertProgFile = NULL;
 static GLuint fragShader;
 static GLuint vertShader;
 static GLuint program;
 static GLint win = 0;
-static GLboolean anim = GL_FALSE;
-static GLfloat xRot = 0.0f, yRot = 0.0f;
 static int w,h;
 
 
@@ -31,23 +27,12 @@ Redisplay(void)
    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
    glBegin(GL_TRIANGLES);
-   glColor3f(.8,0,0); 
    glVertex3f(-0.9, -0.9, 0.0);
-   glColor3f(0,.9,0); 
    glVertex3f( 0.9, -0.9, 0.0);
-   glColor3f(0,0,.7); 
    glVertex3f( 0.0,  0.9, 0.0);
    glEnd();
 
    glutSwapBuffers();
-}
-
-
-static void
-Idle(void)
-{
-   yRot = glutGet(GLUT_ELAPSED_TIME) * 0.1;
-   glutPostRedisplay();
 }
 
 
@@ -77,48 +62,9 @@ CleanUp(void)
 static void
 Key(unsigned char key, int x, int y)
 {
-  (void) x;
-  (void) y;
-
-   switch(key) {
-   case ' ':
-   case 'a':
-      anim = !anim;
-      if (anim)
-         glutIdleFunc(Idle);
-      else
-         glutIdleFunc(NULL);
-      break;
-   case 27:
+   if (key == 27) {
       CleanUp();
       exit(0);
-      break;
-   }
-   glutPostRedisplay();
-}
-
-
-static void
-SpecialKey(int key, int x, int y)
-{
-   const GLfloat step = 3.0f;
-
-  (void) x;
-  (void) y;
-
-   switch(key) {
-   case GLUT_KEY_UP:
-      xRot -= step;
-      break;
-   case GLUT_KEY_DOWN:
-      xRot += step;
-      break;
-   case GLUT_KEY_LEFT:
-      yRot -= step;
-      break;
-   case GLUT_KEY_RIGHT:
-      yRot += step;
-      break;
    }
    glutPostRedisplay();
 }
@@ -139,23 +85,17 @@ Init(void)
    if (!ShadersSupported())
       exit(1);
 
-   if (FragProgFile)
-      fragShader = CompileShaderFile(GL_FRAGMENT_SHADER, FragProgFile);
-   else
-      fragShader = CompileShaderText(GL_FRAGMENT_SHADER, fragShaderText);
+   fragShader = CompileShaderText(GL_FRAGMENT_SHADER, fragShaderText);
 
-   if (VertProgFile)
-      vertShader = CompileShaderFile(GL_VERTEX_SHADER, VertProgFile);
-   else
-      vertShader = CompileShaderText(GL_VERTEX_SHADER, vertShaderText);
+   vertShader = CompileShaderText(GL_VERTEX_SHADER, vertShaderText);
 
    program = LinkShaders(vertShader, fragShader);
 
    glUseProgram(program);
 
-   /*assert(glGetError() == 0);*/
+   assert(glGetError() == 0);
 
-   glClearColor(0.3f, 0.3f, 0.3f, 0.0f);
+   glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
    glEnable(GL_DEPTH_TEST);
 
    printf("GL_RENDERER = %s\n",(const char *) glGetString(GL_RENDERER));
@@ -165,21 +105,6 @@ Init(void)
    assert(glIsShader(vertShader));
 
    glColor3f(1, 0, 0);
-}
-
-
-static void
-ParseOptions(int argc, char *argv[])
-{
-   int i;
-   for (i = 1; i < argc; i++) {
-      if (strcmp(argv[i], "-fs") == 0) {
-         FragProgFile = argv[i+1];
-      }
-      else if (strcmp(argv[i], "-vs") == 0) {
-         VertProgFile = argv[i+1];
-      }
-   }
 }
 
 
@@ -193,11 +118,7 @@ main(int argc, char *argv[])
    glewInit();
    glutReshapeFunc(Reshape);
    glutKeyboardFunc(Key);
-   glutSpecialFunc(SpecialKey);
    glutDisplayFunc(Redisplay);
-   if (anim)
-      glutIdleFunc(Idle);
-   ParseOptions(argc, argv);
    Init();
    glutMainLoop();
    return 0;
